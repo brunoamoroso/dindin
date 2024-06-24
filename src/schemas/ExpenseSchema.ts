1c1b8a155d28b4595acae1bcd78e91fe6983c9db
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-06-24 10:36:45 -0300

    created expense schema

---
+++ src/schemas/ExpenseSchema.ts
@@ -0,0 +1,45 @@
+import { Schema } from "mongoose";
+
+const expenseSchema = new Schema(
+  {
+    description: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    subcategory: {
+      type: String,
+    },
+    account: {
+      type: String,
+      required: true,
+    },
+    payment_method: {
+      type: String,
+      required: true,
+    },
+    payment_terms: {
+      type: String,
+      required: true,
+    },
+    installments: {
+      type: Number,
+    },
+    date_expense: {
+      type: Date,
+      required: true,
+    },
+    recurrency: {
+      type: String,
+      required: true,
+      default: "never",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default expenseSchema;
