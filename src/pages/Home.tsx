6fbc0b5ea6641bca8b97d3f7d2c176382bb6e259
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-06-30 09:33:57 -0300

    Layout, Components, tailwind config
    Developed splash screen for mobile, adjusting tailwind theme for dindin, added button component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="container">
-        <h1>DinDin</h1>
+    <div className="container h-dvh font-sans bg-splash-cover bg-cover text-white">
+      <div className="flex flex-col gap-8 h-full justify-end pb-10">
+        <div>
+          <h1 className="text-5xl font-bold">DinDin</h1>
+          <span className="text-2xl">
+            Sua vida financeira finalmente ficará organizada e equilibrada.
+          </span>
+        </div>
+        <div className="flex flex-col gap-6">
+          <Button variant={'outline'} size={'lg'}>Já tenho conta</Button>
+          <Button size={'lg'}>Criar Conta</Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
