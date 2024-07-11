e5bcfd9827fe6c7ec466430511a8c4ca0fc716fe
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-07-11 20:52:59 -0300

    TextField, handleChange, handleSubmit
    TextField now can receive props for label and input
    
    handleChange saves the data into a state variable
    
    handleSubmit is prepared to send the data to the api endpoint

--- frontend/src/components/ui/textfield.tsx
+++ frontend/src/components/ui/textfield.tsx
@@ -1,11 +1,20 @@
 import { Input } from "./input";
 import { Label } from "./label";
+/**
+ * The basic usage of this component require that you send an id, a label, if it's required
+ * @param 
+ * @returns 
+ */
 
-const TextField = ({id, label} : {id: string; label: string}) => {
-  return (
+interface textFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+
+const TextField = ({...props} : textFieldProps) => {
+  return(
     <div className="flex gap-1.5 flex-col w-full">
-        <Label htmlFor={id}>{label}</Label>
-        <Input id={id} />
+        <Label htmlFor={props.id}>{props.label}</Label>
+        <Input {...props} />
     </div>
   );
 }
--- frontend/src/pages/Profile/CreateProfile.tsx
+++ frontend/src/pages/Profile/CreateProfile.tsx
@@ -2,31 +2,62 @@ import AppBar from "@/components/AppBar";
 import { Button } from "@/components/ui/button";
 import TextField from "@/components/ui/textfield";
 import { Camera } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function CreateProfile() {
+  const [user, setUser] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+
+    setUser((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
     <div className="h-dvh flex flex-col bg-neutral-950">
       <AppBar title="Configurar Conta" />
       <div className="container flex flex-1 py-10">
-        <div className="flex flex-col flex-1 justify-between">
-          <div className="flex flex-col gap-6">
-            <div className="flex flex-col items-center gap-2">
-              <div className="flex bg-primary text-neutral-950 py-11 px-11 rounded-full">
-                <Camera/>
+          <form className="flex flex-col flex-1 justify-between" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center gap-2">
+                <div className="flex bg-primary text-neutral-950 py-11 px-11 rounded-full">
+                  <Camera />
+                </div>
+                <Button variant={"ghost"}>Escolher uma foto</Button>
               </div>
-              <Button variant={'ghost'}>Escolher uma foto</Button>
+
+              <TextField
+                id="name"
+                label="Nome"
+                required={true}
+                onChange={handleChange}
+              />
+              <TextField id="surname" label="Sobrenome" required={true} onChange={handleChange} />
+              <TextField id="email" label="Email" required={true} onChange={handleChange}/>
+              <TextField id="password" label="Senha"  required={true} onChange={handleChange} />
             </div>
 
-            <TextField id="name" label="Nome" />
-            <TextField id="surname" label="Sobrenome" />
-            <TextField id="email" label="Email" />
-            <TextField id="password" label="Senha" />
-          </div>
-
-          <Button size={"lg"} className="w-full">
-            Continuar
-          </Button>
-        </div>
+            <Button
+              size={"lg"}
+              type="submit"
+              className="w-full"
+            >
+              Continuar
+            </Button>
+          </form>
       </div>
     </div>
   );
