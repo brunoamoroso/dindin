116525b1dcdfee9949e1b6dfac2aed712b92d979
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-07-12 10:06:38 -0300

    Express basic server configuration
    Configured a basic server and tested of express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,22 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/create-profile", (req, res) => {
 
+app.get("/", (req, res) => {
+    res.send('hellow world')
+})
+
+app.get("/create-profile", (req, res) => {
+    console.log('oi'); 
 });
 
 app.use((req, res) => {
     res.status(404);
     res.send('Deu 404 aqui');
-})
+});
+
+app.listen(5001, () => {
+    console.log('Server running on: http://localhost:5001')
+}) 
 
 export default app;
