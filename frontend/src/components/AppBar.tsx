3a119612d711e393aff25f05ccea0ef6185b0dde
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-07-13 11:26:10 -0300

    Appbar, Multer
    Adjusted appbar to have a fixed height
    
    Added multer to the backend for fileupload

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -7,7 +7,7 @@ interface IAppBar{
 
 export default function AppBar({title}: IAppBar) {
   return (
-    <div className="flex items-center px-1 py-2 gap-1">
+    <div className="flex items-center h-16 px-1 py-2 gap-1">
         <IconButton variant={"ghost"}><ArrowLeft /></IconButton>
         <span className="title-small text-title">{title}</span>
     </div>
