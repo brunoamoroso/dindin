e28671b4611fb044807bae65852887072a2fdd7d
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-07-21 16:53:26 -0300

    Retrieve subCategories, Handled empty subcategories
    Implemented the subcategories, now when the category has a sub it will show in the page;
    
    Handled when there isn't a subcategory

--- frontend/src/pages/Transactions/SubCategories.tsx
+++ frontend/src/pages/Transactions/SubCategories.tsx
@@ -7,16 +7,19 @@ import api from '@/api/api';
 
 export default function SubCategories() {
   const {category} = useParams();
-  const [subCategories, setSubCategories] = useState([{desc: "", type: ""}]);
+  const [subCategories, setSubCategories] = useState<{desc: string, type: string}[]>([]);
 
   useEffect(() => {
     const getSubCategories = async () => {
-      if(category === undefined){
+      if(category === undefined){ 
         throw new Error("type undefined");
       }
 
-      const response: {status: string, message: []} = await api.getSubCategories(category);
-      setSubCategories(response.message)
+      const response: {status: number, message: []} = await api.getSubCategories(category);
+      console.log(subCategories);
+      if(response.status === 200){
+          setSubCategories(response.message)
+      }
     }
     
     getSubCategories();
@@ -24,25 +27,28 @@ export default function SubCategories() {
 
   return (
     <div className="bg-surface h-dvh">
-      <AppBar title="Escolha uma categoria"/>
+      <AppBar title={`SubCategorias de ${category}`}/>
       <div className="container flex flex-col bg-container2 h-dvh rounded-t-lg py-10">
           {subCategories.map((subCategory, index, arr) => {
             if(arr.length - 1 === index){
               //last item
               return (                
                 <Link to="/" key={index}>
-                  <MenuListItem size="lg" >{subCategory.desc}</MenuListItem>
+                  <MenuListItem size="lg" trailingIcon={false}>{subCategory.desc}</MenuListItem>
                 </Link>
               )
             }else{
               return (                
                 <Link to="/" key={index}>
-                  <MenuListItem size="lg">{subCategory.desc}</MenuListItem>
+                  <MenuListItem size="lg" trailingIcon={false}>{subCategory.desc}</MenuListItem>
                   <Separator />
                 </Link>
               )
             }
           })}
+          {subCategories.length === 0 && (
+            <h1 className="title-large text-title">Essa categoria não possuí nenhuma subcategoria ainda.</h1>
+          )}
       </div>
     </div>
   );
