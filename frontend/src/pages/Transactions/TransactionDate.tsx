2cbbf13905eacaded065f89c1c054ea5095cbafd
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-08-02 19:18:53 -0300

    hotfix: Close Date Modal
    Fixed when clicking to change the month was closing the modal

--- frontend/src/pages/Transactions/TransactionDate.tsx
+++ frontend/src/pages/Transactions/TransactionDate.tsx
@@ -1,27 +1,32 @@
 import { Calendar } from "@/components/ui/calendar";
 import { useTransactionsContext } from "@/hooks/useTransactionsContext";
+import { MouseEvent } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 export default function TransactionDate() {
   const navigate = useNavigate();
-  const {setContextDate, setChipPressed} = useTransactionsContext();
+  const { setContextDate, setChipPressed } = useTransactionsContext();
+
+  const handleOutsideClose = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target !== e.currentTarget) return;
+    navigate("/transaction");
+  };
 
   const handleDayClick = (day: Date) => {
+    console.log(day);
     setContextDate(day);
     setChipPressed("otherDate");
     navigate("/transaction");
-  }
+  };
 
   return (
-    <div className="h-dvh overflow-hidden">  
-      <div className="fixed bg-neutral-950/95 h-dvh w-full flex" onClick={() => {navigate("/transaction")}}>
-          <div className="container flex items-center justify-center">
-              <Calendar onDayClick={handleDayClick}/>
-          </div>
+    <>
+      <div className="fixed bg-neutral-950/95 h-dvh w-full flex container flex items-center justify-center" onClick={handleOutsideClose}>
+        <Calendar onDayClick={handleDayClick} />
       </div>
       <div className="overflow-hidden">
         <Outlet />
       </div>
-    </div>
-  )
+    </>
+  );
 }
