ecd92650adcaa9ef8027d81f14aac1c48a8b69db
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-08-15 11:20:54 -0300

    Added bcrypt, now checks if the email or username is already takent

--- backend/src/controllers/profile-controller.ts
+++ backend/src/controllers/profile-controller.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
+import { query, Request, Response } from "express";
 import clientDB from "../db/conn";
 import e from '../db/dbschema/edgeql-js';
 import { createUserToken } from "../utils/create-user-token";
+import bcrypt from 'bcrypt';
 
 export const CreateProfile = async (req: Request, res:Response) => {
     const {name, surname, email, password, username} = req.body;
@@ -12,6 +13,33 @@ export const CreateProfile = async (req: Request, res:Response) => {
     }
 
     try{
+        const queryEmail = e.select(e.User, () => ({
+            email: true,
+            filter_single: {
+                email: e.str(email)
+            }
+        }));
+
+        const emailExists = await queryEmail.run(clientDB);
+
+        if(emailExists){
+            return res.status(422).json({message: "O email que você utilizou já está cadastrado"});
+        }
+
+        const queryUsername = e.select(e.User, () => ({
+            username: true,
+            filter_single: {
+                username: e.str(username)
+            }
+        }));
+
+        const usernameExists = await queryUsername.run(clientDB);
+
+        if(usernameExists){
+            return res.status(422).json({message: "O nome de usuário já está em uso"})
+        }
+
+
         const insert = e.insert(e.User, {
             photo: e.str(photo),
             name: e.str(name),
