42e30e01c3cbea68bb84f88066afdeece2740e68
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-08-15 10:17:39 -0300

    changed profile controller to use edgeql-js

--- backend/src/controllers/profile-controller.ts
+++ backend/src/controllers/profile-controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import clientDB from "../db/conn";
+import e from '../db/dbschema/edgeql-js';
 
 export const CreateProfile = async (req: Request, res:Response) => {
-    const {name, surname, email, password} = req.body;
+    const {name, surname, email, password, username} = req.body;
     let photo = req.file?.filename;
 
     if(photo === undefined){
@@ -10,23 +11,33 @@ export const CreateProfile = async (req: Request, res:Response) => {
     }
 
     try{
-        const newUser = await clientDB.execute(`
-            insert User {
-                photo := <str>$photo,
-                name := <str>$name,
-                surname := <str>$surname,
-                email := <str>$email,
-                password := <str>$password
-            }`, {
-                photo: photo,
-                name: name,
-                surname: surname,
-                email: email,
-                password: password
-            }).catch((err) => {
-                res.status(422);
-                throw new Error(err);
-            });
+        const insert = e.insert(e.User, {
+            photo: e.str(photo),
+            name: e.str(name),
+            surname: e.str(surname),
+            email: e.str(email),
+            password: e.str(password),
+            username: e.str(username),
+        });
+
+        const newUser = await insert.run(clientDB);
+        // const newUser2 = await clientDB.execute(`
+        //     insert User {
+        //         photo := <str>$photo,
+        //         name := <str>$name,
+        //         surname := <str>$surname,
+        //         email := <str>$email,
+        //         password := <str>$password
+        //     }`, {
+        //         photo: photo,
+        //         name: name,
+        //         surname: surname,
+        //         email: email,
+        //         password: password
+        //     }).catch((err) => {
+        //         res.status(422);
+        //         throw new Error(err);
+        //     });
         console.log(newUser);
     }catch(err: unknown){
         if(err.message.includes("email")){
