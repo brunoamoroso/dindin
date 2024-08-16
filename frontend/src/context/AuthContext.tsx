2fb8c9975b6363cfab28f80bd25913731450bcfd
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-08-16 19:47:23 -0300

    Adjusted Create Profile
    Adjusted create profile to have username field, and when it returns the token from the user creation the auth now recognizes and redirect to the correct page

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
 
 
@@ -12,14 +12,19 @@ export const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthContextProvider = () => {
     const [token, setToken] = useState<string | null>(null);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const cookieToken = Cookies.get('token')
 
         if(cookieToken){
             setToken(cookieToken);
+            if(location.pathname === "/"){
+                navigate("/dashboard");
+            }
         }
-    }, []);
+    }, [token, navigate, location.pathname]);
 
     return(
         <AuthContext.Provider value={{token, setToken}}>
