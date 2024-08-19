d086d39b08f355ab137fd012b062f754876b03dc
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-08-19 08:52:33 -0300

    Update Transactions controller to edgeql-js

--- backend/src/controllers/transactions-controller.ts
+++ backend/src/controllers/transactions-controller.ts
@@ -1,75 +1,99 @@
 import { Request, Response } from "express";
 import clientDB from "../db/conn";
 import { LocalDate } from "edgedb";
+import e from '../db/dbschema/edgeql-js';
 
 export const addTransaction = async (req: Request, res: Response) => {
-    const {type, amount, desc, category, subCategory, account, recurrency, date, paymentMethod, paymentCondition, installments} = req.body;
+    const {type, amount, desc, category, subCategory, account, recurrency, date, paymentMethod, paymentCondition} = req.body;
     const datePart = date.value.split("T");
     const [year, month, day] = datePart[0].split("-").map(Number);
     const localDate = new LocalDate(year, month, day);
+    let installments = parseInt(req.body.installments);
 
-    if(type === "gain"){        
-        try{
-            await clientDB.execute(`
-                    insert Gain {
-                        description := <str>$desc,
-                        amount := <int32>$amount,
-                        category := (select Category filter .id = <uuid>$category),
-                        subCategory := (select subCategory filter .id = <uuid>$subCategory),
-                        account := (select Account filter .id = <uuid>$account),
-                        date_earned := <cal::local_date>$date,
-                        recurrency := <default::Recurrency>$recurrency,
-                        created_by := (select User filter .id = <uuid>$created_by)
-                    }
-                `, {
-                    desc: desc,
-                    amount: amount,
-                    category: category.id,
-                    subCategory: subCategory.id,
-                    account: account.id,
-                    date: localDate,
-                    recurrency: recurrency.id,
-                    created_by: 'd9f7e7e2-42f8-11ef-8903-0b2c32e274a0'
-                });
-        }catch(err){
-            throw new Error(err as string);
+    try{
+        if(type === "gain"){
+            const queryGainTransaction = e.insert(e.Gain, {
+                description: e.str(desc),
+                amount: e.int32(amount),
+                category: e.cast(e.Category, e.uuid(category.id)),
+                subCategory: e.cast(e.subCategory, e.uuid(subCategory.id)),
+                account: e.cast(e.Account, e.uuid(account.id)),
+                recurrency: e.cast(e.Recurrency, recurrency.id),
+                date_earned: e.cal.local_date(localDate),
+                created_by: e.cast(e.User, e.uuid(req.user as string))
+            });
+
+            const gainTransaction = await queryGainTransaction.run(clientDB);
+            res.status(201).json(gainTransaction);
+            return;
         }
-    }
 
-    if(type === "expense"){        
-        try{
-            await clientDB.execute(`
-                    insert Expense {
-                        description := <str>$desc,
-                        amount := <int32>$amount,
-                        category := (select Category filter .id = <uuid>$category),
-                        subCategory := (select subCategory filter .id = <uuid>$subCategory),
-                        account := (select Account filter .id = <uuid>$account),
-                        date_paid := <cal::local_date>$date,
-                        recurrency := <default::Recurrency>$recurrency,
-                        created_by := (select User filter .id = <uuid>$created_by),
-                        payment_method := <str>$paymentMethod,
-                        payment_condition := <str>$paymentCondition,
-                        installments := <int16>$installments
-                    }
-                `, {
-                    desc: desc,
-                    amount: amount,
-                    category: category.id,
-                    subCategory: subCategory.id,
-                    account: account.id,
-                    date: localDate,
-                    recurrency: recurrency.id,
-                    created_by: 'd9f7e7e2-42f8-11ef-8903-0b2c32e274a0',
-                    paymentMethod: paymentMethod,
-                    paymentCondition: paymentCondition,
-                    installments: parseInt(installments)
-                });
+        if(type === "expense"){
+            if(paymentMethod === undefined){
+                return res.status(422).json({message: "Payment Method is mandatory"});
+            }
+
+            if(paymentCondition === undefined){
+                return res.status(422).json({message: "Payment Condition is mandatory"});
+            }
+
+            if(paymentMethod === "credit" && paymentCondition === "single"){
+                installments = 1;
+            }
+
+            const queryExpenseTransaction = e.insert(e.Expense, {
+                description: e.str(desc),
+                amount: e.int32(amount),
+                category: e.cast(e.Category, e.uuid(category.id)),
+                subCategory: e.cast(e.subCategory, e.uuid(subCategory.id)),
+                account: e.cast(e.Account, e.uuid(account.id)),
+                recurrency: e.cast(e.Recurrency, recurrency.id),
+                date_paid: e.cal.local_date(localDate),
+                created_by: e.cast(e.User, e.uuid(req.user as string)),
+                payment_condition: e.str(paymentCondition),
+                payment_method: e.str(paymentMethod),
+                installments: e.int16(installments)
+            });
+
+            const expenseTransaction = await queryExpenseTransaction.run(clientDB);
+            return res.status(201).json(expenseTransaction);
+        }
+
+    // if(type === "expense"){        
+    //     try{
+    //         await clientDB.execute(`
+    //                 insert Expense {
+    //                     description := <str>$desc,
+    //                     amount := <int32>$amount,
+    //                     category := (select Category filter .id = <uuid>$category),
+    //                     subCategory := (select subCategory filter .id = <uuid>$subCategory),
+    //                     account := (select Account filter .id = <uuid>$account),
+    //                     date_paid := <cal::local_date>$date,
+    //                     recurrency := <default::Recurrency>$recurrency,
+    //                     created_by := (select User filter .id = <uuid>$created_by),
+    //                     payment_method := <str>$paymentMethod,
+    //                     payment_condition := <str>$paymentCondition,
+    //                     installments := <int16>$installments
+    //                 }
+    //             `, {
+    //                 desc: desc,
+    //                 amount: amount,
+    //                 category: category.id,
+    //                 subCategory: subCategory.id,
+    //                 account: account.id,
+    //                 date: localDate,
+    //                 recurrency: recurrency.id,
+    //                 created_by: 'd9f7e7e2-42f8-11ef-8903-0b2c32e274a0',
+    //                 paymentMethod: paymentMethod,
+    //                 paymentCondition: paymentCondition,
+    //                 installments: parseInt(installments)
+    //             });
                 
-        }catch(err){
-            throw new Error(err as string);
-        }
+    //     }catch(err){
+    //         throw new Error(err as string);
+    //     }
+    }catch(err){
+        res.status(422).json({message: "Couldn't create the transaction"})
+        throw new Error(err as string);
     }
-
-    return res.status(201).json({message: "Transactron created"});
 }
