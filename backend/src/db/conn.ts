d1f77f669ffefa81bc85b29fb985f6607484ed6e
bruno-connectabil <pamoroso.bruno@gmail.com>
2025-01-18 07:05:53 -0300

    feat: add dockerfile, adjusted conn

--- backend/src/db/conn.ts
+++ backend/src/db/conn.ts
@@ -1,7 +1,10 @@
 import * as edgedb from 'edgedb';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const clientDB = edgedb.createClient({
-    instanceName: "dindindb"
+    dsn: isProd ? process.env.EDGEDB_DSN : undefined,
+    instanceName: !isProd ? 'dindindb' : undefined,
 });
 
 export default clientDB;
