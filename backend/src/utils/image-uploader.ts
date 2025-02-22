e32c4f4a723f72f8c7e97a1399a0788a00d896a8
bruno-connectabil <pamoroso.bruno@gmail.com>
2025-02-22 06:05:39 -0300

    chore: removed commented code

--- backend/src/utils/image-uploader.ts
+++ backend/src/utils/image-uploader.ts
@@ -1,13 +1,4 @@
 import multer from 'multer';
 const storage = multer.memoryStorage();
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, "src/assets/uploads");
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + "-" + String(Math.floor(Math.random() * 1000)) + path.extname(file.originalname));
-//     }
-// });
 const imageUploader = multer({storage: storage});
 export default imageUploader;
